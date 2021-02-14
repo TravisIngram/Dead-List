a27a2f789910073cb387c9a4e7e65fc5fbd0aca7
TravisIngram <ingramtravis@gmail.com>
2021-02-14 16:36:42 -0600

    Add initial routes to class views

---
+++ backend/deadlist/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, path
+from rest_framework import routers
+from .views import *
+
+router = routers.DefaultRouter(trailing_slash=False)
+
+urlpatterns = [
+    path('users/<int:pk>/', UserDetail.as_view()),
+    path('users', UserList.as_view()),
+    path('puns/<int:pk>/', PunDetail.as_view()),
+    path('puns', PunList.as_view()),
+    path('calls/<int:pk>/', CallDetail.as_view()),
+    path('calls', CallList.as_view()),
+    path('deceased/<int:pk>/', DeceasedDetail.as_view()),
+    path('deceased', DeceasedList.as_view()),
+    path('', include(router.urls)),
+]
