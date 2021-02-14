f051744ba050a1363609387dfcdc5c26c91dba05
TravisIngram <ingramtravis@gmail.com>
2021-02-13 23:56:19 -0600

    Add pun, deceased models, update user, call

--- backend/deadlist/models.py
+++ backend/deadlist/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator, URLValidator
 
 # Unused imports from webserver lab...
 # from __future__ import unicode_literals
@@ -26,7 +26,9 @@ class User(models.Model):
     username = models.CharField(max_length=30, blank=False)
     firstName = models.CharField(max_length=30, blank=False)
     lastName = models.CharField(max_length=30, blank=False)
-    email = models.CharField(max_length=30, blank=False)
+    email = models.EmailField(blank=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.username)
@@ -36,13 +38,45 @@ class User(models.Model):
 
 class Call(models.Model):
     title = models.CharField(max_length=100, blank=False)
-    rating = models.IntegerField(
+    callRating = models.IntegerField(
         validators=[MaxValueValidator(5), MinValueValidator(1)])
-    caller = models.ForeignKey(User, on_delete=models.CASCADE)
-    pun = models.ForeignKey(User, on_delete=models.CASCADE)
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    pun = models.ForeignKey(Pun, on_delete=models.CASCADE)
     comment = models.CharField(max_length=300, blank=False)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.title)
+
+# Pun Model - Content, Timestamp, Rating, User (fk), Call (fk)
+# Name, Date of Death, Biography, Link
+
+
+class Pun(models.Model):
+    content = models.CharField(max_length=300, blank=False)
+    punRating = models.IntegerField(
+        validators=[MaxValueValidator(5), MinValueValidator(1)])
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.username)
+
+# Deceased Model - Name, Date of Death, Biography, Link
+
+
+class Deceased(models.Model):
+    name = models.CharField(max_length=200, blank=False)
+    biography = models.CharField(max_length=500, blank=False)
+    link = models.URLField(max_length=300, blank=True,
+                           validators=URLValidator(schemes=['http', 'https']))
+    dateOfDeath = models.CharField(max_length=200, blank=False)
+    punRating = models.IntegerField(
+        validators=[MaxValueValidator(5), MinValueValidator(1)])
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
