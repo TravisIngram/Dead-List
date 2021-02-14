09af7c68e7de8824f002aa3c6ad527ff19d46973
TravisIngram <ingramtravis@gmail.com>
2021-02-14 15:00:56 -0600

    Add remaining initial class views to project

--- backend/deadlist/api/views.py
+++ backend/deadlist/api/views.py
@@ -7,7 +7,7 @@ from deadlist.models import User, Pun, Call, Deceased
 from .serializers import UserSerializer, PunSerializer, CallSerializer, DeceasedSerializer
 
 
-# User routes
+# User routes, Get / Update / Delete a specific user...
 
 
 class UserDetail(APIView):
@@ -54,6 +54,35 @@ class UserList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+# Pun routes, Get / Update / Delete a specific pun...
+
+
+class PunDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        pun = self.get_object(pk)
+        serializer = PunSerializer(pun)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        pun = self.get_object(pk)
+        serializer = PunSerializer(pun, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        pun = self.get_object(pk)
+        pun.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 # Get all puns in DB or create / add a new one
 
 
@@ -66,6 +95,99 @@ class PunList(APIView):
 
     def post(self, request, format=None):
         serializer = PunSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# Call routes, Get / Update / Delete a specific call...
+
+
+class CallDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Call.objects.get(pk=pk)
+        except Call.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        call = self.get_object(pk)
+        serializer = CallSerializer(call)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        call = self.get_object(pk)
+        serializer = CallSerializer(call, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        call = self.get_object(pk)
+        call.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# Get all calls in DB or create / add a new one
+
+
+class CallList(APIView):
+
+    def get(self, request, format=None):
+        calls = Call.objects.all()
+        serializer = CallSerializer(calls, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = CallSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Deceased routes, Get / Update / Delete a specific deceased person...
+
+
+class DeceasedDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        deceased = self.get_object(pk)
+        serializer = DeceasedSerializer(deceased)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        deceased = self.get_object(pk)
+        serializer = DeceasedSerializer(deceased, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        deceased = self.get_object(pk)
+        deceased.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# Get all users in DB or create / add a new one
+
+
+class DeceasedList(APIView):
+
+    def get(self, request, format=None):
+        deceased = Deceased.objects.all()
+        serializer = DeceasedSerializer(deceased, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = DeceasedSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
