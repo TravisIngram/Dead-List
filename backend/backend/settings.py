c6a3c769c0e00b831988d7a4504565bb4978dd9c
TravisIngram <ingramtravis@gmail.com>
2021-02-28 00:47:42 -0600

    Add CORS middleware

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = DEBUG
 
 # Application definition
 
@@ -43,9 +44,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'deadlist',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
