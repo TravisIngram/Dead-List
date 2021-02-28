78623387d22151cb1168410df7731e7e0b4ac453
TravisIngram <ingramtravis@gmail.com>
2021-02-28 14:10:36 -0600

    Add app models to admin page

--- backend/deadlist/admin.py
+++ backend/deadlist/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Call, Pun, Deceased, User
 
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fields = ('username', 'firstName', 'lastName', 'email')
+
+
+@admin.register(Call)
+class CallAdmin(admin.ModelAdmin):
+    fields = ('username', 'deceasedName', 'source',
+              'dateOfDeath', 'comment', 'callRating')
+
+
+@admin.register(Deceased)
+class DeceasedAdmin(admin.ModelAdmin):
+    fields = ('username', 'deceasedName', 'link',
+              'dateOfDeath', 'biography')
+
+
+@admin.register(Pun)
+class PunAdmin(admin.ModelAdmin):
+    fields = ('username', 'punContent', 'punRating', 'call')
+
+    # list_display = ['title', 'author', 'description']
