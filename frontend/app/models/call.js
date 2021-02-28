a8b2f781cbb5a981e3f0be08023f854c24e5e5e8
TravisIngram <ingramtravis@gmail.com>
2021-02-28 14:13:55 -0600

    Add type to attributes

--- frontend/app/models/call.js
+++ frontend/app/models/call.js
@@ -1,10 +1,18 @@
 import Model, { attr } from '@ember-data/model';
 
 export default class CallModel extends Model {
-  @attr username;
-  @attr deceasedName;
-  @attr dateOfDeath;
-  @attr source;
-  @attr callRating;
-  @attr comment;
+  @attr('number') username;
+  @attr('string') deceasedName;
+  @attr('string') dateOfDeath;
+  @attr('string') source;
+  @attr('number') callRating;
+  @attr('string') comment;
 }
+
+
+// @attr username;
+// @attr deceasedName;
+// @attr dateOfDeath;
+// @attr source;
+// @attr callRating;
+// @attr comment;
