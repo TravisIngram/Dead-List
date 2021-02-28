2ff7f8dd2f6932691a2b4e4467a18a7ed053b5a9
TravisIngram <ingramtravis@gmail.com>
2021-02-28 14:12:02 -0600

    Remove RESTAdapter references

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,17 +1,7 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
 
-// import DS from 'ember-data';
-// import { computed } from '@ember/object';
-
 export default class ApplicationAdapter extends JSONAPIAdapter {
   host = 'http://localhost:8000';
   namespace = 'api';
 
 }
-
-// export default DS.RESTAdapter.extend({
-//   host: computed(function(){
-//     return 'http://localhost:8000';
-//   }),
-//   namespace: 'api'
-// });
