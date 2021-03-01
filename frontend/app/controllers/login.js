f986b98b7f90b2caf5b7b59f63feb0804d3726f0
TravisIngram <ingramtravis@gmail.com>
2021-02-28 23:21:32 -0600

    Add initial login and register controllers

---
+++ frontend/app/controllers/login.js
@@ -0,0 +1,15 @@
+// From webservice lab...
+
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	auth: Ember.inject.service('auth-manager'),
+	actions:{
+		login: function(){
+			this.get('auth').login();
+		},
+		logout: function(){
+			this.get('auth').logout();
+		}
+	}
+});
---
+++ frontend/app/controllers/register.js
@@ -0,0 +1,12 @@
+// From webservices lab...
+
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	auth: Ember.inject.service('auth-manager'),
+	actions:{
+		register: function(){
+			this.get('auth').register();
+		}
+	}
+});
